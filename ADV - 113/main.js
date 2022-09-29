001961f0357a5022a9c33bbd5bda56f014130229
kanagarajah20091 <95323647+kanagarajah20091@users.noreply.github.com>
2022-09-29 18:46:28 -0400

    Add files via upload

---
+++ ADV - 113/main.js
@@ -0,0 +1,14 @@
+function preload() {
+
+}
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110,250);
+}
+
+function draw(){
+    circle(350, 50, 50 );
+    rect(30, 120, 50, 100 );
+    ellipse(56, 46, 55, 55);
+}
+
